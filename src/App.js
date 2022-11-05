ae46078db1eb9da3756db32eee6ced25078a2ce0
Muhammad Umair <181370124@gift.edu.pk>
2022-11-05 16:34:08 +0500

    designing UI
    input field and headings in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1>ToDo List</h1>
+        <br />
+        <input type="text" placeholder="Add new item" />
+        <button>+</button>
+        <ol>
+          <li>buy apple</li>
+        </ol>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
