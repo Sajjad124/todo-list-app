4bcd9cdd1bb83e1ecb5eb99d70dce9132a407014
Muhammad Umair <181370124@gift.edu.pk>
2022-11-05 21:18:50 +0500

    state changing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [inputList, setInputList] = useState("Buy Apple");
+
+  const itemEvent=(event)=>{
+     
+  };
+
+  
   return (
     <div className="main_div">
       <div className="center_div">
         <br />
         <h1>ToDo List</h1>
         <br />
-        <input type="text" placeholder="Add new item" />
+        <input type="text" placeholder="Add new item" onChange={itemEvent} />
         <button>+</button>
         <ol>
-          <li>buy apple</li>
+          <li>{inputList}</li>
         </ol>
       </div>
     </div>
